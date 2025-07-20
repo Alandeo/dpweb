406fc77b5e390b7b19526f9631a8190a905605ae
Alandeo <landeoaldair2003@gmail.com>
2025-07-20 17:46:44 -0500

    lande

--- view/function/user.js
+++ view/function/user.js
@@ -106,13 +106,33 @@ async function view_users() {
             mode: 'cors',
             cache: 'no-cache'
         });
-
+        let json = await respuesta.json();
+        if (json && json.length > 0) {
+            let html = '';
+            json.forEach((user, index) => {
+                html += `<tr>
+                <td>${index + 1}</td>
+                <td>${user.nro_identidad || ''}  </td>
+                <td>${user.razon_social || ''}  </td>
+                <td>${user.correo || ''}</td>
+                <td>${user.rol || ''}</td>
+                <td>${user.estado || ''} </td>
+                </tr>`;
+            })
+            document.getElementById('content_users').innerHTML = html;
+        } else {
+            document.getElementById('content_users').innerHTML = '<tr><td colspan="6"> No hay usuarios disponibles</td></tr>';
+        }
     } catch (error) {
-        console.log("Error al obtener usuarios: " + error);
+        console.log(error);
+        document.getElementById('content_users').innerHTML = '<tr><td colspan="6">Error al cargar los usuarios</td></tr>';
     }
 }
+
 if (document.getElementById('content_users')) {
     view_users();
 }
+
+
 
 
