7686063b2b28e55db19e29820391bc96fc73edd9
Alandeo <landeoaldair2003@gmail.com>
2025-09-11 12:50:59 -0500

    Update products.php
    sljjkl

--- view/products.php
+++ view/products.php
@@ -1,1 +1,16 @@
-products
+<div class="container">
+    <h3 class="mt-3 mb-3">Lista de Poductos</h3>
+    <table class="table table-bordered table-striped" >
+        <thead>
+            <tr>
+                <th>nombre</th>
+                <th>detalle</th>
+            </tr>
+        </thead>
+        <tbody id="content_users">
+
+        </tbody>
+    </table>
+</div>
+<script src="<?= BASE_URL ?>view/function/user.js"></script>
+ <!--<script>view_users();</script> -->
