5b91d726b8d856f484634a9e2474521981773887
Alandeo <landeoaldair2003@gmail.com>
2025-11-05 12:54:26 -0500

    1

--- view/include/header.php
+++ view/include/header.php
@@ -53,6 +53,10 @@
                      <li class="nav-item">
                         <a class="nav-link" href="<?= BASE_URL ?>proveedor">🚚proveedor</a>
                     </li>
+                    </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="<?= BASE_URL ?>vista-cliente">vista-cliente</a>
+                    </li>
                 </ul>
                 <form class="d-flex" role="search">
                     <ul class="navbar-nav px-4">
